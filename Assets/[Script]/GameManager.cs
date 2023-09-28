f03d76f67f7ab266ab9d306960985fdf55dd4a2b
KOLIP-LAPTOP\GBC <sinan.kolip@georgebrown.ca>
2023-09-28 10:40:15 -0400

    Score System added

---
+++ Assets/[Script]/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI _scoreText;
+    int _previousScore = 0;
+    [SerializeField]
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(_previousScore != score)
+            UpdateScore();
+        _previousScore = score;
+    }
+
+    public void ChangeSceneToGamePlay()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ChangeScore(int scoreIncrease)
+    {
+        score += scoreIncrease;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        _scoreText.text = "Score: " + score;
+    }
+}
